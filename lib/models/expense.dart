c7bfed8847080cda404cc958108344f598789dc5
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-01-16 23:02:18 +0545

    expense model completed

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,18 +1,16 @@
+import 'package:uuid/uuid.dart';
+
+final uuid = Uuid();
+
 class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+  }) : id = uuid.v4();
 
-Expense ({
-
-required this.title,
-required this.amount,
-required this.date,
-
-
-});
-
-final String id;
-final String title;
-final double  amount;
-final DateTime date;
-
-
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
 }
