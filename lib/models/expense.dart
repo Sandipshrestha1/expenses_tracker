bd80925bf7ce41456703bb45951e212f246822f0
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-01-28 19:41:53 +0545

    adding filter  concept

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -38,24 +38,23 @@ class Expense {
 class ExpenseBucket {
   const ExpenseBucket({required this.category, required this.expenses});
 
+// creating own named constructor .forCategory is an extra constructor
 
-ExpenseBucket.forCategory(List<Expense> allExpenses, this.category );
-
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
 
   final ExpenseCategory category;
   final List<Expense> expenses;
 
 //adding getter to expense bucket
 
-
-double  get totalExpenses {
-
-  double  sum = 0;
-for (final expense in expenses ) {
-
-
-  sum = sum + expense.amount ;  // sum += expense.amount
-}
-  return sum;
-}
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum = sum + expense.amount; // sum += expense.amount
+    }
+    return sum;
+  }
 }
